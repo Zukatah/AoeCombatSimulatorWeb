67f2c9b1754b01c228aa9b19dcd6093df72f1f35
Andreas Beicht <AndreasBeicht@gmx.de>
2020-07-16 01:41:26 +0200

    rewritten parts of the matrix component

--- src/app/matrix/matrix.component.ts
+++ src/app/matrix/matrix.component.ts
@@ -7,6 +7,7 @@ import { Battle } from "./../aoeData/battle";
 import { CivUnitType } from '../aoeData/civUnitType';
 import { AoECombatSimulatorComponent } from '../aoeCombatSimulator/aoeCombatSimulator.component';
 import { Civilization } from '../aoeData/civilization';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 
 @Component({
   selector: 'app-matrix',
@@ -15,67 +16,108 @@ import { Civilization } from '../aoeData/civilization';
 })
 export class MatrixComponent implements OnInit {
 
-	public uts: UnitType[] = AoeData.unitTypesList; // a list of all base unit types in the game (including non-final upgrades such as militia, crossbowmen or non-elite unique units)
+	// hardcoded parts (of the matrix component) //
 	public matrixUts: UnitType[] = []; // a list of all post imp generic civ base unit types and all aok civs' elite unique units, ordered as in the first cost efficiency matrix
-	public combatresults: number[][];
+	public numberUtToDisplay: number;
 	public combatresultsNoHnR: number[][];
 	public combatresultsSemiHnR: number[][];
 	public combatresultsFullHnR: number[][];
 	public combatresultsNoHnR_LGMP: number[][];
+	// end of harcoded parts //
 
-	public championUts: UnitType[] = []
-	public championCivs: Civilization[] = []
+	// dynamically calculated parts (of the matrix component) //
+	public combatresults: number[][];
 	public championCivUts: CivUnitType[] = []
+	public trashCivUts: CivUnitType[] = []
 
-	public hitAndRunMode: number = 0; // 0=noHit&Run, 1=semi, 2=fullHit&Run
+	public hitAndRunMode: number = 1; // 0=noHit&Run, 1=semi, 2=fullHit&Run
 	public hitAndRunModes: string[] = ["No Hit&Run", "Medium Hit&Run", "Perfect Hit&Run"];
 
-	public resourceValue: number = 0; // 0=equal worth, 1=gold+50% worth, 2=100f=100w=17g
+	public resourceValue: number = 2; // 0=equal worth, 1=gold+50% worth, 2=100f=100w=17g
 	public resourceValues: string[] = ["100F=100W=100G", "100F=100W=66,6G", "100F=100W=17G"];
 	public resourceValuesFactors: number[][] = [[1, 1, 1], [1, 1, 1.5], [1, 1, 5.88235]];
 
-	public numberOfSimulations: number = 20;
+	public numberOfSimulations: number = 40;
 	public ut1: number = 0;
 	public ut2: number = 0;
+	public showAvgCol: boolean = true;
 
 	public players: Player[];
 
-	public numberUtToDisplay: number = 15; // to display all unit types in the matrix, set this to 57; to display all non-unique unit types, set this to 15
+	public numberUtToDisplayRows: number; // to display all unit types in the matrix, set this to 57; to display all non-unique unit types, set this to 15
+	public numberUtToDisplayColumns: number; // to display all unit types in the matrix, set this to 57; to display all non-unique unit types, set this to 15
+	// end of dynamically calculated parts //
 
 
 	constructor() {
-		this.championCivUts.push(
-			new CivUnitType(AoeData.ut_champion, AoeData.civ_chinese), new CivUnitType(AoeData.ut_champion, AoeData.civ_berbers),
-			new CivUnitType(AoeData.ut_champion, AoeData.civ_aztecs), new CivUnitType(AoeData.ut_twoHandedSwordsman, AoeData.civ_bulgarians),
-			new CivUnitType(AoeData.ut_champion, AoeData.civ_burmese), new CivUnitType(AoeData.ut_champion, AoeData.civ_byzantines),
-			new CivUnitType(AoeData.ut_champion, AoeData.civ_celts), new CivUnitType(AoeData.ut_champion, AoeData.civ_goths),
-			new CivUnitType(AoeData.ut_champion, AoeData.civ_japanese),	new CivUnitType(AoeData.ut_twoHandedSwordsman, AoeData.civ_malay),
-			new CivUnitType(AoeData.ut_champion, AoeData.civ_malians), new CivUnitType(AoeData.ut_champion, AoeData.civ_portuguese),
-			new CivUnitType(AoeData.ut_champion, AoeData.civ_slavs), new CivUnitType(AoeData.ut_champion, AoeData.civ_teutons),
-			new CivUnitType(AoeData.ut_champion, AoeData.civ_vikings),
-		);
-
-
-		// uncomment to calculate matrix (commented out to get pre-calculated values)
-		this.combatresults = [];
-		this.numberUtToDisplay = this.championCivUts.length;
-		for(let i: number = 0; i < this.numberUtToDisplay; i++){
-			this.combatresults.push([]);
-			for (let j: number = 0; j < this.numberUtToDisplay; j++){
-				this.combatresults[i].push(i == j ? 1 : 0);
-			}
-			this.combatresults[i].push(0);
-		}
-		setTimeout(this.CreateBattles.bind(this), 0);
-		
 		this.matrixUts.push(AoeData.ut_villager, AoeData.ut_champion, AoeData.ut_halberdier, AoeData.ut_eliteEagleWarrior,
 			AoeData.ut_hussar, AoeData.ut_paladin, AoeData.ut_heavyCamelRider, AoeData.ut_eliteBattleElephant, AoeData.ut_eliteSteppeLancer,
 			AoeData.ut_arbalester, AoeData.ut_eliteSkirmisher, AoeData.ut_heavyCavalryArcher, AoeData.ut_handCannoneer,
 			AoeData.ut_siegeRam, AoeData.ut_heavyScorpion,
 			AoeData.ut_eliteLongbowman, AoeData.ut_eliteCataphract, AoeData.ut_eliteWoadRaider, AoeData.ut_eliteChuKoNu, AoeData.ut_eliteThrowingAxeman,
 			AoeData.ut_eliteHuskarl, AoeData.ut_eliteSamurai, AoeData.ut_eliteMangudai, AoeData.ut_eliteWarElephant, AoeData.ut_eliteMameluke,
-			AoeData.ut_eliteTeutonicKnight, AoeData.ut_eliteJanissary, AoeData.ut_eliteBerserk);
+			AoeData.ut_eliteTeutonicKnight, AoeData.ut_eliteJanissary, AoeData.ut_eliteBerserk
+		);
+		this.numberUtToDisplay = this.matrixUts.length;
 
+
+		this.championCivUts.push(
+			new CivUnitType(AoeData.ut_twoHandedSwordsman, AoeData.civ_bulgarians),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_teutons),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_slavs),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_aztecs),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_burmese),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_japanese),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_vikings),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_goths),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_celts),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_berbers),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_portuguese),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_byzantines),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_malians),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_chinese),
+			new CivUnitType(AoeData.ut_twoHandedSwordsman, AoeData.civ_malay)
+		);
+		/*this.trashCivUts.push(
+			new CivUnitType(AoeData.ut_twoHandedSwordsman, AoeData.civ_bulgarians),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_teutons),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_slavs),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_aztecs),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_burmese),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_japanese),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_vikings),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_goths),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_celts),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_berbers),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_portuguese),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_byzantines),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_malians),
+			new CivUnitType(AoeData.ut_champion, AoeData.civ_chinese),
+			new CivUnitType(AoeData.ut_twoHandedSwordsman, AoeData.civ_malay)
+		);*/
+		this.trashCivUts.push(
+			new CivUnitType(AoeData.ut_halberdier, AoeData.civ_spanish),
+			new CivUnitType(AoeData.ut_hussar, AoeData.civ_spanish),
+			new CivUnitType(AoeData.ut_eliteMagyarHuszar, AoeData.civ_magyars),
+			new CivUnitType(AoeData.ut_eliteSkirmisher, AoeData.civ_spanish),
+			new CivUnitType(AoeData.ut_eliteGenitour, AoeData.civ_berbers),
+			new CivUnitType(AoeData.ut_imperialSkirmisher, AoeData.civ_vietnamese),
+			new CivUnitType(AoeData.ut_crossbowman, AoeData.civ_persians),
+		);
+
+		// uncomment to calculate matrix (commented out to get pre-calculated values)
+		this.combatresults = [];
+		this.numberUtToDisplayRows = this.championCivUts.length;
+		this.numberUtToDisplayColumns = this.trashCivUts.length;
+		for(let i: number = 0; i < this.numberUtToDisplayRows; i++){
+			this.combatresults.push([]);
+			for (let j: number = 0; j < this.numberUtToDisplayColumns; j++){
+				this.combatresults[i].push((this.championCivUts[i].civ == this.trashCivUts[j].civ && this.championCivUts[i].baseUnitType.name == this.trashCivUts[j].baseUnitType.name) ? 1 : 0);
+			}
+			this.combatresults[i].push(0);
+		}
+		setTimeout(this.CreateBattles.bind(this), 0);
+		
 	}
 
 
@@ -208,11 +250,11 @@ export class MatrixComponent implements OnInit {
 
 	public CreateBattles(){
 
-		if (this.ut1 != this.ut2){
+		if (this.championCivUts[this.ut1].civ != this.trashCivUts[this.ut2].civ || this.championCivUts[this.ut1].baseUnitType.name != this.trashCivUts[this.ut2].baseUnitType.name){
 			this.players = [new Player(new Color(0, 0, 128), 0), new Player(new Color(0, 0, 128), 1)];
 
 			this.players[0].civUts = this.championCivUts;
-			this.players[1].civUts = this.championCivUts;
+			this.players[1].civUts = this.trashCivUts;
 			this.players[0].ResetData();
 			this.players[1].ResetData();
 
@@ -228,22 +270,21 @@ export class MatrixComponent implements OnInit {
 			}
 			this.CalculateStats();
 			this.combatresults[this.ut1][this.ut2] = this.players[0].resourcesLostTotal != 0 ? this.players[1].resourcesLostTotal / this.players[0].resourcesLostTotal : Number.POSITIVE_INFINITY;
-			console.log(this.ut1 + " " + this.ut2 + ": " + this.combatresults[this.ut1][this.ut2] + " (" + this.players[0].resourcesLostTotal + " / " + this.players[1].resourcesLostTotal + ")");
+			// console.log(this.ut1 + " " + this.ut2 + ": " + this.combatresults[this.ut1][this.ut2] + " (" + this.players[0].resourcesLostTotal + " / " + this.players[1].resourcesLostTotal + ")");
 		}
 
 		this.ut2++;
-		if (this.ut2 == this.numberUtToDisplay){
+		if (this.ut2 == this.numberUtToDisplayColumns){
 			let avgResult = 0.0;
-			for (let i: number = 0; i < this.numberUtToDisplay; i++){
+			for (let i: number = 0; i < this.numberUtToDisplayColumns; i++){
 				avgResult += this.combatresults[this.ut1][i];
 			}
-			console.log("avg " + avgResult);
-			this.combatresults[this.ut1][this.numberUtToDisplay] = avgResult / this.numberUtToDisplay;
+			this.combatresults[this.ut1][this.numberUtToDisplayColumns] = avgResult / this.numberUtToDisplayColumns;
 			this.ut2 = 0;
 			this.ut1++;
 		}
 		
-		if (this.ut1 < this.numberUtToDisplay){
+		if (this.ut1 < this.numberUtToDisplayRows){
 			setTimeout(this.CreateBattles.bind(this), 20);
 		} else {
 			let outputstring: string = "";
@@ -251,6 +292,18 @@ export class MatrixComponent implements OnInit {
 				outputstring = outputstring.concat("[" + arr + "],\n");
 			})
 			console.log(outputstring);
+
+			let tempSortList = [];
+			for (let row = 0; row < this.numberUtToDisplayRows; row++){
+				tempSortList.push({'civUnitType': this.championCivUts[row], 'cR': this.combatresults[row]});
+			}
+			tempSortList.sort(function(a, b){
+				return (a.cR[a.cR.length-1] < b.cR[b.cR.length-1] ? 1 : (a.cR[a.cR.length-1] == b.cR[b.cR.length-1] ? 0 : -1));
+			});
+			for (let row = 0; row < this.numberUtToDisplayRows; row++){
+				this.championCivUts[row] = tempSortList[row].civUnitType;
+				this.combatresults[row] = tempSortList[row].cR;
+			}
 		}
 	}
 
